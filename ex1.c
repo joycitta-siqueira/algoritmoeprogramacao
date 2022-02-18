2a5f71ca62178d3221990ef72a7b8276960e1134
joycitta-siqueira <joycitta@gmail.com>
2022-02-18 09:56:11 -0300

    primeiro algoritmo

---
+++ ex1.c
@@ -0,0 +1,14 @@
+/* O seu primeiro programa em qualquer linguagem de programação 
+normalmente é o "Hello World!". Neste primeiro problema tudo o que 
+você precisa fazer é imprimir esta mensagem na tela. */
+//comentando uma única linha
+/* comentando um bloco de linhas */
+
+#include<stdio.h> //biblioteca para funções de entrada e saida de dados
+
+int main(){ //inicio do algoritmo
+
+	printf("Hello World!");
+	
+}//final do algorimo
+
